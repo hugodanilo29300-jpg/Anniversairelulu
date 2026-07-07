62200dd9a01f30ea6e3fdb5ea5d26c78095e01d7
hugodanilo29300-jpg <hugodanilo29300@gmail.com>
2026-07-07 16:25:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,29 @@
-// ==============================
-// NOTRE UNIVERS ❤️
-// Version 1
-// ==============================
+const bouton = document.getElementById("startButton");
 
-const startButton = document.getElementById("startButton");
+bouton.addEventListener("click", () => {
 
-const today = new Date();
+    document.body.style.transition = "1s";
 
-const hours = today.getHours();
+    document.body.style.opacity = "0";
 
-let message = "";
+    setTimeout(() => {
 
-if (hours < 12) {
-    message = "☀️ Bonjour mon cœur ❤️";
-} else if (hours < 18) {
-    message = "💜 Coucou Lulu ❤️";
-} else {
-    message = "🌙 Bonne soirée mon cœur ❤️";
-}
-
-const subtitle = document.querySelector(".subtitle");
-
-subtitle.innerHTML = message + "<br><br>31 jours pour te rappeler combien je t'aime.";
-
-startButton.addEventListener("click", () => {
-
-    alert(
-`❤️ Bienvenue dans Notre Univers ❤️
+        alert(
+`❤️ Bonjour mon cœur ❤️
 
 Aujourd'hui commence une aventure.
 
-Chaque jour, une nouvelle enveloppe t'attendra.
+Pendant les 31 prochains jours...
+
+Une nouvelle surprise t'attendra.
 
 Je t'aime.
 
 — Hugo ❤️`
-    );
+        );
+
+        document.body.style.opacity = "1";
+
+    },1000);
 
 });
